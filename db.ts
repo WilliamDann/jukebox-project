4529a1cbe13a3a8ceabd5fe51fd2ada5b8bbfcab
WilliamDann <wcdann2@gmail.com>
2025-06-09 13:30:31 -0700

    changed to postgres

--- db.ts
+++ db.ts
@@ -7,58 +7,47 @@ export default function(): Promise<Client|null>
     return new Promise((resolve, reject) => {
         // read the script to create the DB
         const initScript = `
-            -- table storing user accounts
-            create table if not exists accounts (
-                id           int          unique not null auto_increment,
-                email        varchar(255) unique not null,
-                displayName  varchar(255) not null,
-                passwordHash varchar(255) not null,
+        -- create the schema if it doesn't exist
+        create schema if not exists jukeboxdb;
 
-                primary key (id)
-            );
+        -- switch to the schema (in PostgreSQL you set the search_path)
+        set search_path to jukeboxdb;
 
-            -- table storing access tokens to our application
-            --  this is not access tokens to spotify
-            create table if not exists accessTokens (
-                accessToken     varchar(255) unique not null,
-                accountId       int not null,
+        -- table storing user accounts
+        create table if not exists accounts (
+            id serial primary key,
+            email varchar(255) unique not null,
+            displayName varchar(255) not null,
+            passwordHash varchar(255) not null
+        );
 
-                primary key(accessToken),
-                foreign key(accountId) references accounts(id)
-            );
+        -- table storing access tokens to our application
+        -- this is not access tokens to spotify
+        create table if not exists accessTokens (
+            accessToken varchar(255) primary key,
+            accountId int not null references accounts(id)
+        );
 
-            -- a user profile belongs to a user under an account who has connected their spotify
-            create table if not exists profiles (
-                id              int unique not null auto_increment,
-                displayName     varchar(255) not null,
-                accountId       int not null,
-                active          boolean not null default false,
+        -- a user profile belongs to a user under an account who has connected their spotify
+        create table if not exists profiles (
+            id serial primary key,
+            displayName varchar(255) not null,
+            accountId int not null references accounts(id),
+            active boolean not null default false,
+            spotAuthToken varchar(255)
+        );
 
-                -- spotify assigns an auth code to every connected account
-                --  this is different than the token that performs actions.
-                --  this code is never refreshed and is created when the account is linked
-                spotAuthToken   varchar(255),      
-
-                primary key(id),
-                foreign key(accountId) references accounts(id)       
-            );
-
-            -- a spotify access token is what allows us to take actions on a user's behalf
-            --  there are generated when we need then and they expire after some time
-            create table if not exists spotifyAccessTokens (
-                -- info returned by spotify
-                access_token         varchar(255) not null,
-                refresh_token        varchar(255) not null,
-                expires_in           int not null,
-                scope                varchar(255) not null,
-
-                -- our info
-                profileId           int not null,
-                generatedAt         bigint not null,
-
-                primary key(access_token),
-                foreign key(profileId) references profiles(id)
-            )`
+        -- a spotify access token is what allows us to take actions on a user's behalf
+        -- they are generated when we need them and expire after some time
+        create table if not exists spotifyAccessTokens (
+            access_token varchar(255) primary key,
+            refresh_token varchar(255) not null,
+            expires_in int not null,
+            scope varchar(255) not null,
+            profileId int not null references profiles(id),
+            generatedAt bigint not null
+        );
+        `
     
         // connect to local mysql database
         //  for development this is fine but when the app is released
